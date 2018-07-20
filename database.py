6c528d3bac2f61f4df57b89fb4414d0a0b63a0ba
Maeve <m1229567@gmail.com>
2018-07-20 15:16:29 -0500

    Added Event class

--- database.py
+++ database.py
@@ -1,1 +1,12 @@
-# This will hold our datastore classes
+from google.appengine.ext import ndb
+
+class Event(ndb.Model):
+    name = ndb.StringProperty()
+    location = ndb.StringProperty()
+    date = ndb.DateProperty(auto_now=True)
+    time = ndb.TimeProperty(auto_now=True)
+    user_rating = ndb.IntegerProperty()
+    price_level = ndb.IntegerProperty()
+
+
+class Schedule(ndb.Model):
