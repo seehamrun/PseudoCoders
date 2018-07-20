9ca26dcf83a76ca9e11dabf0875087554ab57e76
Maeve <m1229567@gmail.com>
2018-07-20 17:12:27 -0500

    ignore this

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 from google.appengine.ext import ndb
 
+
 class Event(ndb.Model):
     name = ndb.StringProperty()
     location = ndb.StringProperty()
@@ -7,7 +8,6 @@ class Event(ndb.Model):
     time = ndb.TimeProperty(auto_now_add=True)
     user_rating = ndb.IntegerProperty()
     price_level = ndb.IntegerProperty()
-
 
 class Schedule(ndb.Model):
     events = ndb.StructuredProperty(Event, repeated=True)
