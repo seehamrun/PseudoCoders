0b5ce01963174a44be76a0e999f399a1f8006cba
Ethan Mann <e.milomann@gmail.com>
2018-07-24 13:05:27 -0500

    Revert "past changes"
    This reverts commit bea7e3e2b15cec8cd1e816f20a092da09f25ccd8.

--- static/script.js
+++ static/script.js
@@ -3,15 +3,18 @@
 //this will be all of our non-API js code
 function submitClick() {
   var loader1 = document.querySelector('#futureLoader')
-  // loader1.classList.add("loader")
+  //loader1.classList.add("loader")
   loader1.classList.add("cssload-cube")
   loader1.innerHTML = "<div class='cssload-face cssload-x'></div><div class='cssload-face cssload-y'></div><div class='cssload-face cssload-z'></div>"
 
   var loader2 = document.querySelector('#loadingMessage')
   loader2.innerHTML = "Your results are loading...!"
 
-  var everythingElse = document.querySelector(".regular")
-  everythingElse.style.display = "none"
+  var everythingElse = document.querySelectorAll(".regular")
+  for (var i=0; i<everythingElse.length; i++)
+  {
+    everythingElse[i].classList.add("disappear")
+  }
 }
 
 
