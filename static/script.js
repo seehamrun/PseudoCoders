756ec86954793550b61c8e71bd65cc2a3b374fdf
Ethan Mann <ethanmann@Ethans-MacBook-Air-2.local>
2018-07-23 21:42:39 -0500

    cool loader

--- static/script.js
+++ static/script.js
@@ -2,8 +2,13 @@
 
 //this will be all of our non-API js code
 function submitClick() {
-  var loader = document.querySelector('#futureLoader')
-  loader.classList.add("loader")
+  var loader1 = document.querySelector('#futureLoader')
+  //loader1.classList.add("loader")
+  loader1.classList.add("cssload-cube")
+  loader1.innerHTML = "<div class='cssload-face cssload-x'></div><div class='cssload-face cssload-y'></div><div class='cssload-face cssload-z'></div>"
+
+  var loader2 = document.querySelector('#loadingMessage')
+  loader2.innerHTML = "Your results are loading...!"
 }
 
 window.addEventListener('load', () => {
