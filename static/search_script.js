b7066aa3e46ae98f3071385d67f1d8a4cb91f595
Ethan Mann <e.milomann@gmail.com>
2018-07-25 08:36:14 -0500

    added video to loading animation

--- static/search_script.js
+++ static/search_script.js
@@ -2,12 +2,20 @@
 
 //this will be all of our non-API js code
 function submitClick() {
-  var loader1 = document.querySelector('#futureLoader')
-  loader1.classList.add("cssload-cube")
-  loader1.innerHTML = "<div class='cssload-face cssload-x'></div><div class='cssload-face cssload-y'></div><div class='cssload-face cssload-z'></div>"
+  // var loader1 = document.querySelector('#futureLoader')
+  // loader1.classList.add("cssload-cube")
+  // loader1.innerHTML = "<div class='cssload-face cssload-x'></div><div class='cssload-face cssload-y'></div><div class='cssload-face cssload-z'></div>"
+  //
+  // var loader2 = document.querySelector('#loadingMessage')
+  // loader2.style.display = "Block"
 
-  var loader2 = document.querySelector('#loadingMessage')
-  loader2.style.display = "Block"
+  var text = document.querySelector('#loaderText')
+  text.style.display = "Block"
+
+  var video = document.querySelector('#loaderVideo')
+  video.style.display = "Block"
+  // loader1.classList.add("cssload-cube")
+  // loader1.innerHTML = "<div class='cssload-face cssload-x'></div><div class='cssload-face cssload-y'></div><div class='cssload-face cssload-z'></div>"
 
   var everythingElse = document.querySelector(".regular")
   everythingElse.style.display = "None"
