abbfcdba1c0f94ac1a34dc15e681c3a684e69f6b
Ethan Mann <e.milomann@gmail.com>
2018-07-25 08:47:20 -0500

    video animation autoplay

--- static/search_script.js
+++ static/search_script.js
@@ -12,10 +12,10 @@ function submitClick() {
   var text = document.querySelector('#loaderText')
   text.style.display = "Block"
 
+  var videoWrapper = document.querySelector('#wrapper')
+  videoWrapper.style.display = "Block"
   var video = document.querySelector('#loaderVideo')
-  video.style.display = "Block"
-  // loader1.classList.add("cssload-cube")
-  // loader1.innerHTML = "<div class='cssload-face cssload-x'></div><div class='cssload-face cssload-y'></div><div class='cssload-face cssload-z'></div>"
+  video.src = "https://www.youtube.com/embed/D-nE_HXjnZw?autoplay=1"
 
   var everythingElse = document.querySelector(".regular")
   everythingElse.style.display = "None"
