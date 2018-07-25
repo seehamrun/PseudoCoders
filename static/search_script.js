c953a994691988a03fce842716291d915a02749e
Ethan Mann <e.milomann@gmail.com>
2018-07-25 08:57:15 -0500

    revert animation

--- static/search_script.js
+++ static/search_script.js
@@ -2,20 +2,20 @@
 
 //this will be all of our non-API js code
 function submitClick() {
-  // var loader1 = document.querySelector('#futureLoader')
-  // loader1.classList.add("cssload-cube")
-  // loader1.innerHTML = "<div class='cssload-face cssload-x'></div><div class='cssload-face cssload-y'></div><div class='cssload-face cssload-z'></div>"
+  var loader1 = document.querySelector('#futureLoader')
+  loader1.classList.add("cssload-cube")
+  loader1.innerHTML = "<div class='cssload-face cssload-x'></div><div class='cssload-face cssload-y'></div><div class='cssload-face cssload-z'></div>"
+
+  var loader2 = document.querySelector('#loadingMessage')
+  loader2.style.display = "Block"
+
+  // var text = document.querySelector('#loaderText')
+  // text.style.display = "Block"
   //
-  // var loader2 = document.querySelector('#loadingMessage')
-  // loader2.style.display = "Block"
-
-  var text = document.querySelector('#loaderText')
-  text.style.display = "Block"
-
-  var videoWrapper = document.querySelector('#wrapper')
-  videoWrapper.style.display = "Block"
-  var video = document.querySelector('#loaderVideo')
-  video.src = "https://www.youtube.com/embed/D-nE_HXjnZw?autoplay=1"
+  // var videoWrapper = document.querySelector('#wrapper')
+  // videoWrapper.style.display = "Block"
+  // var video = document.querySelector('#loaderVideo')
+  // video.src = "https://www.youtube.com/embed/D-nE_HXjnZw?autoplay=1"
 
   var everythingElse = document.querySelector(".regular")
   everythingElse.style.display = "None"
