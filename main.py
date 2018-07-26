54ef3be59a5da772299229ce19c377ce5b08b368
Ethan Mann <e.milomann@gmail.com>
2018-07-26 10:54:18 -0500

    new change

--- main.py
+++ main.py
@@ -33,6 +33,9 @@ class FavoritesHandler(webapp2.RequestHandler):
             userFavorites.current = 0
             userFavorites.put()
 
+        if len(userFavorites.favorites) == 0:
+            return webapp2.redirect('/favorites_empty.html')
+
         userFavoritesList = database.UserFavorites.query(database.UserFavorites.userID == users.get_current_user().user_id()).fetch()
         # try:
         userFavorites = userFavoritesList[0]
